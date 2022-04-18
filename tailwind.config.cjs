98d4d772bd220e93354f2db2ec4da5e876976556
alvaaz <alvaro@goede.cl>
2022-04-18 14:49:55 -0400

    design issues fixed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}', 'dist/index.html'],
 	theme: {
 		extend: {
       background: {
